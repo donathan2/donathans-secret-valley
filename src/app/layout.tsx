aaba5246fd6056c185f741d6aebd817a61d99689
donathan2 <donathan.truong1@gmail.com>
2025-06-30 07:18:39 -0400

    chore: favicon stuff

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,9 @@ import { normalFont } from "./lib/fonts";
 export const metadata = {
   title: "Donathan's Secret Valley",
   description: "Donathan's personal website",
+  icons: {
+    icon: "/favicon.ico", // You can also use PNGs like "/favicon.png"
+  },
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
